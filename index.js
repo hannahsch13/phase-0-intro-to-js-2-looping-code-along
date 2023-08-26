0eacbaaae9c91c31c00655bdb7177bb9d518103a
Hannah Schwaeble <hannah.schwaeble@gmail.com>
2023-08-26 16:14:31 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+const friends = ["Guadalupe", "Ollie", "Aki"]
+const event = "surprise"
+function writeCards(friends, event) {
+    let message = []
+  for (let i = 0; i < friends.length; i++) {
+    message.push(`Thank you, ${friends[i]}, for the wonderful ${[event]} gift!`)
+  }
+  return message;
+}
+
+function countDown(i){
+    while (i>= 0) {
+    console.log(i--);
+    }
+}  
